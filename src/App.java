c3a63f6a16b73a268f7dfb105d7b9a81338b13ba
CesarMeche <120508307+CesarMeche@users.noreply.github.com>
2025-03-07 13:35:57 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -3,12 +3,7 @@ import model.PalindromValidator;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        PalindromValidator m = new PalindromValidator();
-        String palin = "radar./,/ o ,.radar";
-        String notPalin = "radar ,. so radar";
-        System.out.println(m.isPalindrom(notPalin));
-        System.out.println(m.isPalindrom(palin));
-        SimpleList sp= new SimpleList();
+        
         
     }
 }
